8e449709544d8313696e9da7aa346e5a67c45abf
Francisco Silva <franjs.francisco@gmail.com>
2022-11-25 17:22:55 +0100

    removing comments

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,9 +15,9 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'tdf-labs', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  organizationName: 'tdf-labs', // GitHub org/user name.
+  projectName: 'docs', // Repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
