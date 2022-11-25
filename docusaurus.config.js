a49c9ae65937e02bc6e893e879bd207609107d0e
Fei Liu <liu.feiwood@gmail.com>
2022-11-25 20:48:08 +0100

    modify github edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,46 +1,45 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'TDF-Labs Docs',
-  tagline: 'Empowering web3 builders',
-  url: 'https://tdf-labs.github.io',
-  baseUrl: '/docs',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "TDF-Labs Docs",
+  tagline: "Empowering web3 builders",
+  url: "https://tdf-labs.github.io",
+  baseUrl: "/docs",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
-  organizationName: 'tdf-labs', // GitHub org/user name.
-  projectName: 'docs', // Repo name.
+  organizationName: "tdf-labs", // GitHub org/user name.
+  projectName: "docs", // Repo name.
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/tdf-labs/docs/',
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/tdf-labs/docs/blob/main",
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -50,56 +49,56 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'TDF-Labs Docs',
+        title: "TDF-Labs Docs",
         logo: {
-          alt: 'tdf-labs-logo',
-          src: 'img/tdf-logo-black.png',
+          alt: "tdf-labs-logo",
+          src: "img/tdf-logo-black.png",
         },
         items: [
           {
-            type: 'doc',
-            docId: '/category/getting-started',
-            position: 'left',
-            label: 'Get Started',
+            type: "doc",
+            docId: "/category/getting-started",
+            position: "left",
+            label: "Get Started",
           },
           {
-            href: 'https://github.com/tdf-labs',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/tdf-labs",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Get Started',
-                to: '/',
+                label: "Get Started",
+                to: "/",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: '#',
+                label: "Discord",
+                href: "#",
               },
               {
-                label: 'Twitter',
-                href: '#',
+                label: "Twitter",
+                href: "#",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/tdf-labs',
+                label: "GitHub",
+                href: "https://github.com/tdf-labs",
               },
             ],
           },
